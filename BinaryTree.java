8086357a0c2a1291e0b46c1f2fbd441b90d8b9d2
Jose Roque <jose.roque@mavs.uta.edu>
2017-09-19 23:46:29 -0500

    Add generic Binary Tree

---
+++ BinaryTree.java
@@ -0,0 +1,46 @@
+public class BinaryTree<K extends Comparable, V> {
+  BinaryTreeNode<K,V> root;
+
+  public BinaryTree() {
+    this.root = null;
+  }
+
+  public void addBinaryTreeNode( K key, V value ) {
+    BinaryTreeNode<K,V> n = new BinaryTreeNode<K,V>(key, value);
+    if( root == null ) {
+      root = n;
+      return;
+    }
+    addBinaryTreeNode( root, n );
+  }
+
+  public void addBinaryTreeNode( BinaryTreeNode<K,V> node, BinaryTreeNode<K,V> data ) {
+      if(node != null && data.key.compareTo(node.key) < 0) {
+        if( node.left == null ){
+          node.left = data;
+        } else {
+          addBinaryTreeNode(node.left, data);
+        }
+      } else if(node != null && data.key.compareTo(node.key) > 0){
+        if( node.right == null ){
+          node.right = data;
+        } else {
+          addBinaryTreeNode(node.right, data);
+        }
+      }
+
+  }
+  public void printBinaryTree() {
+    printBinaryTree(root);
+  }
+
+  private void printBinaryTree(BinaryTreeNode<K,V> p) {
+    if( p == null) {
+      return;
+    }
+
+    printBinaryTree(p.left);
+    System.out.println(p.value);
+    printBinaryTree(p.right);
+  }
+} // end Tree
---
+++ BinaryTreeNode.java
@@ -0,0 +1,26 @@
+public class BinaryTreeNode<K extends Comparable, V> implements Comparable<BinaryTreeNode<K, V>> {
+  K key;
+  V value;
+  BinaryTreeNode<K, V> parent;
+  BinaryTreeNode<K, V> left;
+  BinaryTreeNode<K, V> right;
+
+  public BinaryTreeNode(K key, V value) {
+    this.key = key;
+    this.value = value;
+    this.parent = null;
+    this.left = null;
+    this.right = null;
+  }
+
+  @Override
+  public int compareTo(BinaryTreeNode<K, V> o) {
+    if(o == null) {
+        return 0;
+    }
+    if( o.getClass() != this.getClass() ) {
+        return 0;
+    }
+    return this.key.compareTo(o.key);
+  }
+}
